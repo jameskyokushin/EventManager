058068bbfc3e34298f070ca8940fce51fbaa5694
jameskyokushin <james@kyokushin.com.ph>
2012-03-30 16:31:21 +0800

    done with step 1 and 2

--- event_manager.rb
+++ event_manager.rb
@@ -14,8 +14,19 @@ class EventManager
   		puts line.inspect
   	end
   end
+
+  def print_numbers
+  	@file.each do |line|
+  		number = line[:homephone]
+  		clean_number = number.delete("-")
+  		puts clean_number
+
+	end
+  end
 end
 
 # Script
 manager = EventManager.new
-manager.print_names
+# manager.print_names
+manager.print_numbers
+
