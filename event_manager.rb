c3f53c005eea84898fdf16491541fddb51fcfedc
jameskyokushin <james@kyokushin.com.ph>
2012-03-30 16:20:13 +0800

    test

---
+++ event_manager.rb
@@ -0,0 +1,21 @@
+# Dependencies
+require "csv"
+
+# Class Definition
+class EventManager
+  def initialize
+    puts "EventManager Initialized."
+    filename = "event_attendees.csv"
+    @file = CSV.open(filename, { :headers => true, :header_converters => :symbol })
+  end
+
+  def print_names
+  	@file.each do |line|
+  		puts line.inspect
+  	end
+  end
+end
+
+# Script
+manager = EventManager.new
+manager.print_names
