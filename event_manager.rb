1a34d9379efe6f755705f6f2709d06ab22320660
jameskyokushin <james@kyokushin.com.ph>
2012-03-31 13:03:17 +0800

    done on step3

--- event_manager.rb
+++ event_manager.rb
@@ -18,7 +18,18 @@ class EventManager
   def print_numbers
     @file.each do |line|
       number = line[:homephone]
-      clean_number = number.delete("-")
+      clean_number = number.delete("-,., ,(,)")
+      if number.length == 10 
+         number = clean_number
+      elsif number.length == 11
+        if number.start_with?("1")
+          number = number[1..-1]
+        else
+          number = "0000000000"
+        end
+      else
+        number = "0000000000"
+      end
       puts clean_number
     end
   end
